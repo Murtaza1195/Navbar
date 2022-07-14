b9ceb14046c8e229b25cd5d900b00c82f1dd634d
Murtaza1195 <murtazabangash718@gmail.com>
2022-07-14 17:25:40 +0500

    commit
    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,11 +12,11 @@ const Navbar = () => {
         <a href="#"><img src= {logo} className="logo" alt="Logo"/></a>
         <div>
             <ul  className={showMediaIcons? " menu-link mobile-menu-link" : "menu-link"}>
-                <li><a className ="active" href="#">Home</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Blog</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
+                <li><a className='active'  href="#">Home</a></li>
+                <li><a  href="#">Services</a></li>
+                <li><a  href="#">Blog</a></li>
+                <li><a  href="#">About</a></li>
+                <li><a  href="#">Contact</a></li>
             </ul>
         </div>
         <div className='hamburger-menu'>
