71b8661b52a57984a045cc85ba7759453527a4aa
Murtaza1195 <murtazabangash718@gmail.com>
2022-07-20 16:52:53 +0500

    commit
    new changes

--- src/components/Infocard.jsx
+++ src/components/Infocard.jsx
@@ -3,42 +3,44 @@ import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { SocialMediaIconsReact } from "social-media-icons-react";
 import { Socialdata } from "../Socialicon";
 import { Carddata } from "../data";
-import '../index.css'
+import "../index.css";
 
 const Infocard = () => {
   return (
     <>
-      <div className="cardInfo ">
-        <div className="card d-inline">
-          {Carddata.map((props) => {
-            return (
-              <>
-                <img src={props.img} class="card-img-top" alt="img" />
-                <div className="card-body">
-                  <h5 className="card-title">{props.title}</h5>
-                  <div className="card-text">
-                    <p>{props.Phone}</p>
-                    <p>{props.Email}</p>
-                    <p>{props.Gender}</p>
-                  </div>
-                </div>
-              </>
-            );
-          })}
-
-          <div className="Social-link">
-            {Socialdata.map((item) => {
+      <div className="row">
+        <div className="col-md-4">
+          <div className="card text-center">
+            {Carddata.map((props) => {
               return (
-                <SocialMediaIconsReact
-                  id={item.id}
-                  borderColor={item.borderColor}
-                  icon={item.icon}
-                  iconColor={item.iconColor}
-                  backgroundColor={item.backgroundColor}
-                  url={item.url}
-                  size={item.size}
-                  margin={item.margin}
-                />
+                <>
+                  <img src={props.img} class="card-img-top" alt="img" />
+                  <div className="card-body">
+                    <h5 className="card-title">{props.title}</h5>
+                    <div className="card-text">
+                      <p>{props.Phone}</p>
+                      <p>{props.Email}</p>
+                      <p>{props.Gender}</p>
+
+                      <a href='#'>
+                        {Socialdata.map((item) => {
+                          return (
+                            <SocialMediaIconsReact
+                              id={item.id}
+                              borderColor={item.borderColor}
+                              icon={item.icon}
+                              iconColor={item.iconColor}
+                              backgroundColor={item.backgroundColor}
+                              url={item.url}
+                              size={item.size}
+                              margin={item.margin}
+                            />
+                          );
+                        })}
+                      </a>
+                    </div>
+                  </div>
+                </>
               );
             })}
           </div>
